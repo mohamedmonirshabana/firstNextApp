06b5ce5f287dc7ba1c0b491459b97aaa13db8a65
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-13 14:40:25 +0300

    add Link anchor for about

--- app/page.js
+++ app/page.js
@@ -5,6 +5,9 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <a href="/about">About us</a>
+      </p>
     </main>
   );
 }
